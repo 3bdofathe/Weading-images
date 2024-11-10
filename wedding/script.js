d41b291effefd4aa2f416a12eb8360572be5fc02
abdelrahim fathy <92693596+3bdofathe@users.noreply.github.com>
2024-11-10 15:49:22 +0200

    Add files via upload

---
+++ wedding/script.js
@@ -0,0 +1,34 @@
+
+VanillaTilt.init(document.querySelectorAll(".tilt"), {
+    max: 15,
+});
+// end 
+
+
+const srtop = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 1000,
+    reset: true
+});
+srtop.reveal('.containar .txt h1', { delay: 800 });
+srtop.reveal('.containar .txt a', { delay: 1300 });
+srtop.reveal('.about .about1', { delay: 200 });
+srtop.reveal('.about .about2', { delay: 400 });
+srtop.reveal('.about .about3 p', { delay: 800 });
+srtop.reveal('.about a', { delay: 1000 });
+srtop.reveal('.couple .cop1', { delay: 200 });
+srtop.reveal('.couple .cop2', { delay: 800 });
+srtop.reveal('.gallary .gal1', { delay: 200 });
+srtop.reveal('.gallary .gal2', { delay: 400 });
+srtop.reveal('.gallary .gal3', { delay: 600 });
+srtop.reveal('.conact h1', { delay: 200 });
+srtop.reveal('.conact .cont .cont1', { delay: 800 });
+srtop.reveal('.conact .cont .cont2', { delay: 400 });
+
+srtop.reveal('.footer .foot .foot1', { delay: 200 });
+srtop.reveal('.footer .foot .foot2', { delay: 400 });
+srtop.reveal('.footer .foot .foot3', { delay: 600 });
+srtop.reveal('.footer .foot .foot4', { delay: 800 });
+srtop.reveal('.footer .foot .foot5', { delay: 1000 });
+srtop.reveal('.footer .made', { delay: 1200 });
